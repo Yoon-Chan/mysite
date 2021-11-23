0b1586487c199abaea6ea054c5bce2519b5f746c
vsvx1 <vsvx13@naver.com>
2021-11-23 15:02:42 +0900

    404페이지 만드는 법

--- pybo/views.py
+++ pybo/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Question
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 def index(request):
@@ -18,6 +18,6 @@ def detail(request, question_id):
     """
         pybo 내용 출력
     """
-    question = Question.objects.get(id=question_id)
+    question = get_object_or_404(Question, pk=question_id) #Question.objects.get(id=question_id)
     context = {'question' : question}
     return render(request, 'pybo/question_detail.html', context)
