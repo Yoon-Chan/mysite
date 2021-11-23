e157fc422a293e6898cfcb1952b2c267af42f17e
vsvx1 <vsvx13@naver.com>
2021-11-23 14:30:34 +0900

    admin 모델 사용 및 검색 기능

--- pybo/admin.py
+++ pybo/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Question
 
 # Register your models here.
+
+
+
+#장고 Admin에 데이터 검색 기능 추가하기
+class QuestionAdmin(admin.ModelAdmin):
+    search_fields = ['subject']
+
+#장고 admin에 Question 모델 추가하기.
+admin.site.register(Question,QuestionAdmin)
