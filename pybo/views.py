509282a89699d4146c1a62510903318a8500f0c8
vsvx1 <vsvx13@naver.com>
2021-12-01 22:29:16 +0900

    게시물 수정 & 삭제 기능 추가1 답변 수정기능

--- pybo/views.py
+++ pybo/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Question
+from .models import Question,Answer
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .forms import QuestionForm, AnswerForm
@@ -132,3 +132,28 @@ def question_delete(request, question_id):
         return redirect('pybo:detail', question_id=question_id)
     question.delete()
     return redirect('pybo:index')
+
+@login_required(login_url='common:login')
+def answer_modify(request, answer_id):
+    """
+        pybo 답변 수정
+    """
+    answer = get_object_or_404(Answer, pk=answer_id)
+    if request.user != answer.author:
+        messages.error(request, '수정 권한이 없습니다.')
+        return redirect('pybo:detail', question_id = answer.question_id)
+
+    if request.method == "POST":
+        form = AnswerForm(request.POST, instance=answer)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.author = request.user
+            answer.modify_date = timezone.now()
+            answer.save()
+            return redirect('pybo:detail', question_id = answer.question_id)
+
+    else:
+        form = AnswerForm(instance=answer)
+    context = {'answer' :answer, 'form': form}
+    return render(request, 'pybo/answer_form.html', context)
+
