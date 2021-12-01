cc93b206416763fb0002bb3e60296cb650ce11df
vsvx1 <vsvx13@naver.com>
2021-12-01 21:32:18 +0900

    게시물 수정 & 삭제 기능 추가1 질문 수정 기능

--- pybo/views.py
+++ pybo/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .forms import QuestionForm, AnswerForm
 from django.contrib.auth.decorators import  login_required
+from django.contrib import messages
 
 #페이지 기능 구현하기 위한 사용 모듈
 from django.core.paginator import  Paginator
@@ -88,4 +89,34 @@ def question_create(request):
     else:
         form = QuestionForm()
     context = {'form' : form}
+    return render(request, 'pybo/question_form.html', context)
+
+@login_required(login_url='common:login')
+def question_modify(request, question_id):
+    """
+     pybo 질문 수정
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    if request.user != question.author:
+        #messages 모듈은 장고가 제공하는 함수로 오류를 임의로 발생시키고 싶은 경우에 사용한다.
+        #이때 임의로 발생시킨 오류는 폼필드와 관련이 없으므로 넌필드 오류에 해당된다.
+        messages.error(request, '수정 권한이 없습니다.')
+        return redirect('pybo:detail', question_id= question_id)
+
+    if request.method =="POST":
+        #POST 요청으로 수정 내용을 반영하는 경우에는 다음과 같은 폼을 생성해야한다.
+        #아래 코드는 question을 기본값으로 하여 화면으로 전달받은 입력값들을 덮어써서 QuestionForm을 생성하라는 의미.
+        form = QuestionForm(request.POST, instance=question)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.author = request.user
+            question.modify_date = timezone.now()
+            question.save()
+            return redirect('pybo:detail', question_id=question_id)
+    else:
+        #GET 요청으로 질문 수정 화면이 나타날 때 기존에 저장되어 있던 제목, 내용이 반영된 상태에서
+        #수정을 시작할 수 있도록 다음과 같이 폼을 생성했다.
+        #이처럼 instance 매개변수에 question을 지정하면 기존 값을 폼에 채울 수 있다.
+        form = QuestionForm(instance=question)
+    context = {'form': form}
     return render(request, 'pybo/question_form.html', context)
