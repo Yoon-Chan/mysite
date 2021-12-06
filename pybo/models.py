58367ffc7a6aa76680c02c7643aaed0b217e3766
vsvx1 <vsvx13@naver.com>
2021-12-06 18:17:18 +0900

    댓글 model class(데이터베이스) 설정

--- pybo/models.py
+++ pybo/models.py
@@ -26,3 +26,15 @@ class Answer(models.Model):
     # null=True는 데이터베이스에서 modify_date 칼럼에 null을 허용한다는 의미
     # blank= True는 Form.is_valid()를 통한 입력 폼 데이터 검사 시 값이 없어도 된다는 의미이다.
     modify_date = models.DateTimeField(null=True, blank=True)
+
+#답변 클래스, Question or Answer 필드 중 하나에만 값이 저장되므로 두 필드는 모두
+#null =True, blank =True여야 한다.
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    create_date = models.DateTimeField()
+    modify_date = models.DateTimeField(null=True, blank=True)
+    question = models.ForeignKey(Question, null=True, blank=True,
+                                 on_delete=models.CASCADE)
+    answer = models.ForeignKey(Answer, null=True, blank=True, on_delete=models.CASCADE)
+
