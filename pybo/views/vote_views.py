cb709e5c542775b22e4024bcea43eba18a410cc7
vsvx1 <vsvx13@naver.com>
2021-12-14 22:37:54 +0900

    추천 기능 추가하기3 댓글 추천하기. 본인 댓글 추천하면 에러 표시 상대방 추천시 숫자 증가. 중복 추가가 안된다.(ManyToManyField 속성때문)

--- pybo/views/vote_views.py
+++ pybo/views/vote_views.py
@@ -2,7 +2,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404,redirect
 
-from ..models import Question
+from ..models import Question,Answer
 
 
 # ManyToManyFeild는 같은사용자가 하나의 질문을 여러 번 추천해도 추천 수가 증가하지 않는다.
@@ -19,4 +19,16 @@ def vote_question(request, question_id):
     else:
         question.voter.add(request.user)
     return redirect('pybo:detail', question_id=question_id)
+
+@login_required(login_url='common:login')
+def vote_answer(request, answer_id):
+    """
+        pybo 답변 추천 등록
+    """
+    answer = get_object_or_404(Answer, pk= answer_id)
+    if request.user == answer.author:
+        messages.error(request,'본인이 작성한 글은 추천할 수 없습니다.')
+    else:
+        answer.voter.add(request.user)
+    return redirect('pybo:detail', question_id = answer.question.id)
 
